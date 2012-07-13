c5e03fa0949e7546811c562e99b266724a65e4c2
surinder-anattadesign <surinder7997@gmail.com>
2012-07-13 21:23:04 +0530

    design implemented on homepage

--- wp-content/themes/blocco/foreword.php
+++ wp-content/themes/blocco/foreword.php
@@ -3,28 +3,33 @@
 ?>
 
 <div id="foreword">
-	<?php if ($disable_theme_features['2'] == '0') { ?>
+
 	<div id="logo">
 		<a class="fade" href="<?php echo home_url(); ?>/" title="Home" ><img src="<?php echo ($logo = of_get_option('logo_image')) ? $logo : get_template_directory_uri() . "/images/logo.png"; ?>" alt="<?php bloginfo('name'); ?>" /></a>
 	</div>
-	<?php } ?>
+	
 	
 	<div id="menu">
-		<ul class="menu home-menu">
-			<li class="<?php if (is_front_page()) { echo 'current-menu-item'; } ?>"><a href="<?php echo home_url(); ?>/"><?php echo of_get_option('home_text'); ?></a></li>
-		</ul>
-		
+		<?php if (!is_front_page()) { ?>
+			<ul class="menu home-menu">
+				<li class="<?php if (is_front_page()) { echo 'current-menu-item'; } ?>"><a href="<?php echo home_url(); ?>/"><?php echo of_get_option('home_text'); ?></a></li>
+			</ul>
+		<?php  } ?>
 		<?php if ($disable_theme_features['1'] == '0') { ?>
 		<form method="get" id="search-form" action="<?php echo home_url(); ?>/">
 			<input type="text" name="s" id="s" value="<?php echo of_get_option('search_text'); ?>" onfocus="if(this.value=='<?php echo of_get_option('search_text'); ?>')this.value='';" onblur="if(this.value=='')this.value='<?php echo of_get_option('search_text'); ?>';" />
 		</form>
 		<?php } ?>
 		
-		<?php wp_nav_menu(array('theme_location' => 'first_menu', 'container_class' => 'first-menu')); ?>
+		<?php if (!is_front_page()) { ?>
 		
-		<?php wp_nav_menu(array('theme_location' => 'second_menu', 'container_class' => 'second-menu')); ?>
+			<?php wp_nav_menu(array('theme_location' => 'first_menu', 'container_class' => 'first-menu')); ?>
+			
+			<?php wp_nav_menu(array('theme_location' => 'second_menu', 'container_class' => 'second-menu')); ?>
+			
+			<?php wp_nav_menu(array('theme_location' => 'third_menu', 'container_class' => 'third-menu')); ?>
 		
-		<?php wp_nav_menu(array('theme_location' => 'third_menu', 'container_class' => 'third-menu')); ?>
+		<?php  } ?>
 		
 		<?php if (of_get_option("enable_home_category")) : else : if (function_exists('sf_pagenavi')) { sf_pagenavi('', '', '', '', 20, false); } endif; ?>
 	</div> <!-- menu -->
--- wp-content/themes/blocco/index.php
+++ wp-content/themes/blocco/index.php
@@ -2,6 +2,9 @@
 
 <div id="content" class="<?php if (is_home()) { echo 'is-home'; } ?>">
 	<div id="content-gallery">
+	
+	<?php mylinkorder_list_bookmarks('orderby=order&category_orderby=order'); ?>
+	
 		<?php if (is_home()) {
 			if (of_get_option("enable_home_category")) { 
 				$home_cat = of_get_option('home_category');
@@ -10,11 +13,13 @@
 			};
 		}; ?>
 	
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php /* if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php get_template_part( 'content', 'gallery' ); ?>
 		<?php endwhile; else : ?>
 			<?php get_template_part( 'content', 'nothing' ); ?>
-		<?php endif; ?>
+		<?php endif; */?>
+		
+		
 	</div> <!-- content-gallery -->
 </div> <!-- content -->
 
